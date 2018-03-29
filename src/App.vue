e853de287cdff5ee75437d2f89118f8d93cc9021
goodliang <swl8806@126.com>
2018-03-29 21:24:55 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <div id="app">
+    <loading v-model="isLoading"></loading>
     <router-view></router-view>
+    <input type="text" v-model="isLoading">
   </div>
 </template>
-
 <script>
+import { Loading } from 'vux'
+import { mapState } from 'vuex'
 export default {
-  name: 'app'
+  name: 'app',
+  computed: {
+    ...mapState(['isLoading'])
+  },
+  components: {
+    Loading
+  }
 }
-</script>
 
+</script>
 <style lang="less">
 @import '~vux/src/styles/reset.less';
 
 body {
   background-color: #fbf9fe;
 }
+
 </style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,45 +1,24 @@
 <template>
-<div>
-  <div v-transfer-dom>
-      <loading v-model="isLoading"></loading>
-    </div>
-  <divider>zh-CN: 我是有底线的</divider>
-  <router-link to="/404">我是404</router-link> 
-</div>
+  <div>
+    <divider>zh-CN: 我是有底线的</divider>
+    <router-link to="/404">我是404</router-link>
+  </div>
 </template>
-
-
 <script>
-import { Divider,Loading ,TransferDom} from 'vux'
+import { Divider } from 'vux'
 
 export default {
-  directives: {
-    TransferDom
-  },
-  data () {
+  data() {
     return {
-      showMenu: false,
-      menus: {
-        'language.noop': '<span class="menu-title">Language</span>',
-        'zh-CN': '中文',
-        'en': 'English'
-      },
-      drawerVisibility: false,
-      showMode: 'push',
-      showModeValue: 'push',
-      showPlacement: 'left',
-      showPlacementValue: 'left'
+      showMenu: false
     }
   },
   computed: {
-    isLoading(){
-      return this.$store.state.isLoading
-    }
+
   },
   components: {
-    Divider,
-    Loading,
-    TransferDom
+    Divider
   }
 }
+
 </script>
